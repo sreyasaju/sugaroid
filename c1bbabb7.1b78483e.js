5e77bec31b0d0fff077e6e2fc47f786d8f006168
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-19 21:03:24 +0300

    Deploy website - based on f2ec6d63b23818721fd8a3a294e4ec79d86e1552

---
+++ c1bbabb7.1b78483e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{127:function(o){o.exports=JSON.parse('{"allTagsPath":"/sugaroid/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/sugaroid/blog/tags/hello"}')}}]);
